5cc962567b4c5efdf7bfa53185b49ce4cf60b2a0
Kevin Chaudhari <kevin@Kevins-MBP.home>
2020-04-30 22:09:55 -0400

    Shell script seems to work, have a bunch of sample datapoints in the output folder

--- collect.sh
+++ collect.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-for r in {1000..100000..1000}
+for r in $(seq 100 100 1000)
 do
-	for c in {10..1000..10}
+	for c in {1..10}
 	do
 		start=`date +%s`
 		docker run --net=host weaveworksdemos/load-test -h localhost -r $r -c $c
 		end=`date +%s`
 		runtime=$((end-start))
-		python query_csv_all.py http://localhost:9090 $runtime | gzip > $(date +"%Y_%m_%d_%I_%M_%p")_metrics.gz
+		python query_csv_all.py http://localhost:9090 $runtime | gzip > output/$(date +"%Y_%m_%d_%I_%M_%S_%p")_metrics.gz
+		
 	done
 done
