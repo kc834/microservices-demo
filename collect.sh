f7a685cfdbe5f4103ee227ec3a4156e740abafdf
vedantapawar <vedantapawar@gmail.com>
2020-05-05 13:10:56 -0400

    Collect sh with all loads

--- collect.sh
+++ collect.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
+start=`date +%s`
 for r in $(seq 100 100 1000)
 do
 	for c in {1..10}
 	do
-		start=`date +%s`
 		docker run --net=host weaveworksdemos/load-test -h localhost -r $r -c $c
 		end=`date +%s`
-		runtime=$((end-start))
-		python query_csv_all.py http://localhost:9090 $runtime | gzip > output/$(date +"%Y_%m_%d_%I_%M_%S_%p")_metrics.gz
-		
 	done
 done
+runtime=$((end-start))
+python3 query_csv_all.py http://localhost:9090 $runtime | gzip > output/$(date +"%Y_%m_%d_%I_%M_%S_%p")_metrics.gz
+
